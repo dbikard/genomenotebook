7795145918ed3544f516f2653a009094b6d53348
David Bikard <dbikard@gmail.com>
2023-04-18 14:53:31 +0200

    fix js inclusion

--- genomenotebook/js_callback_code.py
+++ genomenotebook/js_callback_code.py
@@ -4,14 +4,14 @@ import os
 module_path = os.path.abspath(__file__)
 
 # Construct the path to the file you need to open
-file_path = os.path.join(os.path.dirname(module_path), "x_range_change_callback_code.js")
+file_path = os.path.join(os.path.dirname(module_path), "javascript/x_range_change_callback_code.js")
 
 # Open the file and read its contents
 with open(file_path, 'r') as handle:
     x_range_change_callback_code = ''.join(handle.readlines())
 
 # Construct the path to the file you need to open
-file_path = os.path.join(os.path.dirname(module_path), "search_callback_code.js")
+file_path = os.path.join(os.path.dirname(module_path), "javascript/search_callback_code.js")
 
 with open(file_path, 'r') as handle:
     search_callback_code =''.join(handle.readlines())
