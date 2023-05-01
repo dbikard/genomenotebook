c47cd5b4b2c616e196447249432fec2337d00644
David Bikard <dbikard@gmail.com>
2023-05-01 19:26:33 +0200

    hide bokeh banner

--- genomenotebook/__init__.py
+++ genomenotebook/__init__.py
@@ -4,4 +4,4 @@ from .js_callback_code import get_example_data_dir
 from .browser import GenomeBrowser
 from .utils import get_genome_annotations, get_genes_from_annotation
 from bokeh.io import output_notebook
-output_notebook() 
+output_notebook(hide_banner=True) 
